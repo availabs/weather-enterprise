105035b3d688e916e15206b589b1d37882a32545
Krans <ekrans@albany.edu>
2017-01-18 16:59:45 -0500

    'pictures'

--- src/components/Sections/IntroSlider.js
+++ src/components/Sections/IntroSlider.js
@@ -9,7 +9,7 @@ export const IntroSlider = () => (
           {/* SLIDE  */}
           <li className='dark-bg' data-transition='fade' data-slotamount={5} data-masterspeed={700}>
             {/* MAIN IMAGE */}
-            <img src='img/full/24.jpg' alt='slidebg1' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
+            <img src='img/full/rain1.jpg' alt='slidebg1' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
             {/* LAYER NR. 1 */}
             <div className='tp-caption lfb ltt tp-resizeme' data-x='center' data-hoffset={0} data-y='center' data-voffset={-125} data-speed={600} data-start={500} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 2, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
               <img src='img/t-logo1.png' alt='logo' />
@@ -28,89 +28,6 @@ export const IntroSlider = () => (
             </div>
           </li>
           {/* SLIDE  */}
-          <li className='dark-bg' data-transition='fade' data-slotamount={5} data-masterspeed={700}>
-            {/* MAIN IMAGE */}
-            <img src='img/full/30.jpg' alt='slidebg2' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
-            {/* LAYER NR. 1 */}
-            <div className='tp-caption lfb ltt tp-resizeme' data-x='center' data-hoffset={0} data-y='center' data-voffset={-125} data-speed={600} data-start={500} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 2, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <img src='img/t-logo1.png' alt='logo' />
-            </div>
-            {/* LAYER NR. 2 */}
-            <h2 className='tp-caption lfb ltt tp-resizeme sc-title' data-x='center' data-hoffset={0} data-y='center' data-voffset={-25} data-speed={600} data-start={700} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>Flexible &amp; Customizable
-            </h2>
-            {/* LAYER NR. 3 */}
-            <p className='tp-caption lfb ltt tp-resizeme sc-paragraph-lead' data-x='center' data-hoffset={0} data-y='center' data-voffset={40} data-speed={600} data-start={900} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              A donec sodales sagittis magna. Sed consequat, leo eget bibendum amet nibh<br />
-              sodales augue velit cursus nunc.
-            </p>
-            {/* LAYER NR. 4 */}
-            <div className='tp-caption lfb ltt tp-resizeme sc-button' data-x='center' data-hoffset={0} data-y='center' data-voffset={125} data-speed={600} data-start={1100} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <a className='btn btn-md btn-black'>Shop Now</a>
-            </div>
-          </li>
-          {/* SLIDE  */}
-          <li className='dark-bg' data-transition='fade' data-slotamount={5} data-masterspeed={700}>
-            {/* MAIN IMAGE */}
-            <img src='img/full/31.jpg' alt='slidebg3' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
-            {/* LAYER NR. 1 */}
-            <div className='tp-caption lfb ltt tp-resizeme' data-x='center' data-hoffset={0} data-y='center' data-voffset={-125} data-speed={600} data-start={500} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 2, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <img src='img/t-logo1.png' alt='logo' />
-            </div>
-            {/* LAYER NR. 2 */}
-            <h2 className='tp-caption lfb ltt tp-resizeme sc-title' data-x='center' data-hoffset={0} data-y='center' data-voffset={-25} data-speed={600} data-start={700} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>I Am a John Doe
-            </h2>
-            {/* LAYER NR. 3 */}
-            <p className='tp-caption lfb ltt tp-resizeme sc-paragraph-lead' data-x='center' data-hoffset={0} data-y='center' data-voffset={40} data-speed={600} data-start={900} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              A donec sodales sagittis magna. Sed consequat, leo eget bibendum amet nibh<br />
-              sodales augue velit cursus nunc.
-            </p>
-            {/* LAYER NR. 4 */}
-            <div className='tp-caption lfb ltt tp-resizeme sc-button' data-x='center' data-hoffset={0} data-y='center' data-voffset={125} data-speed={600} data-start={1100} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <a className='btn btn-md btn-black'>Shop Now</a>
-            </div>
-          </li>
-          {/* SLIDE  */}
-          <li className='dark-bg' data-transition='fade' data-slotamount={5} data-masterspeed={700}>
-            {/* MAIN IMAGE */}
-            <img src='img/full/32.jpg' alt='slidebg4' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
-            {/* LAYER NR. 1 */}
-            <div className='tp-caption lfb ltt tp-resizeme' data-x='center' data-hoffset={0} data-y='center' data-voffset={-125} data-speed={600} data-start={500} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 2, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <img src='img/t-logo1.png' alt='logo' />
-            </div>
-            {/* LAYER NR. 2 */}
-            <h2 className='tp-caption lfb ltt tp-resizeme sc-title' data-x='center' data-hoffset={0} data-y='center' data-voffset={-25} data-speed={600} data-start={700} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>Welcome Studio
-            </h2>
-            {/* LAYER NR. 3 */}
-            <p className='tp-caption lfb ltt tp-resizeme sc-paragraph-lead' data-x='center' data-hoffset={0} data-y='center' data-voffset={40} data-speed={600} data-start={900} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              A donec sodales sagittis magna. Sed consequat, leo eget bibendum amet nibh<br />
-              sodales augue velit cursus nunc.
-            </p>
-            {/* LAYER NR. 4 */}
-            <div className='tp-caption lfb ltt tp-resizeme sc-button' data-x='center' data-hoffset={0} data-y='center' data-voffset={125} data-speed={600} data-start={1100} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <a className='btn btn-md btn-black'>Shop Now</a>
-            </div>
-          </li>
-          {/* SLIDE  */}
-          <li className='dark-bg' data-transition='fade' data-slotamount={5} data-masterspeed={700}>
-            {/* MAIN IMAGE */}
-            <img src='img/full/35.jpg' alt='slidebg5' data-bgfit='cover' data-bgposition='top center' data-bgrepeat='no-repeat' />
-            {/* LAYER NR. 1 */}
-            <div className='tp-caption lfb ltt tp-resizeme' data-x='center' data-hoffset={0} data-y='center' data-voffset={-125} data-speed={600} data-start={500} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 2, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <img src='img/t-logo1.png' alt='logo' />
-            </div>
-            {/* LAYER NR. 2 */}
-            <h2 className='tp-caption lfb ltt tp-resizeme sc-title' data-x='center' data-hoffset={0} data-y='center' data-voffset={-25} data-speed={600} data-start={700} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>I Am a John Doe
-            </h2>
-            {/* LAYER NR. 3 */}
-            <p className='tp-caption lfb ltt tp-resizeme sc-paragraph-lead' data-x='center' data-hoffset={0} data-y='center' data-voffset={40} data-speed={600} data-start={900} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              A donec sodales sagittis magna. Sed consequat, leo eget bibendum amet nibh<br />
-              sodales augue velit cursus nunc.
-            </p>
-            {/* LAYER NR. 4 */}
-            <div className='tp-caption lfb ltt tp-resizeme sc-button' data-x='center' data-hoffset={0} data-y='center' data-voffset={125} data-speed={600} data-start={1100} data-easing='Power4.easeOut' data-splitin='none' data-splitout='none' data-elementdelay='0.01' data-endelementdelay='0.1' data-endspeed={500} data-endeasing='Power4.easeIn' style={{zIndex: 3, maxWidth: 'auto', maxHeight: 'auto', whiteSpace: 'nowrap'}}>
-              <a className='btn btn-md btn-black'>Shop Now</a>
-            </div>
-          </li>
         </ul>
         <div className='tp-bannertimer' />
       </div>
